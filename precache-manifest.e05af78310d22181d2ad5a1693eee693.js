a8bc572be15d3a17f8304e68d0c28caead917027
Iman Akbar <imanakbar2910@gmail.com>
2020-10-22 05:45:47 +0700

    trial 1

---
+++ precache-manifest.e05af78310d22181d2ad5a1693eee693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6f014df538f4f8490ae",
+    "url": "/static/css/main.0bf00ee3.chunk.css"
+  },
+  {
+    "revision": "d6f014df538f4f8490ae",
+    "url": "/static/js/main.d6f014df.chunk.js"
+  },
+  {
+    "revision": "d255074f6f9bc8237968",
+    "url": "/static/js/1.d255074f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ce58b6be5bc3b12659ac6d336e3d429",
+    "url": "/index.html"
+  }
+];
